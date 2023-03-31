a1d24548b182eb6679fc08cd58a96f631049470a
nemet92 <pasayev.nemet@mail.ru>
2023-04-01 02:17:50 +0400

    sql

--- lib/gridview.dart
+++ lib/gridview.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:loginsql/KatagorilerModel.dart';
 
@@ -15,94 +13,129 @@ class Gridview1 extends StatefulWidget {
 class _Gridview1State extends State<Gridview1> {
   List<KatagoriModel> modelim = [];
   // List<KatagorilerApi> modelli = [];
+  KatagorilerApi api = KatagorilerApi();
 
   @override
   void initState() {
     super.initState();
-    KatagorilerApi api = KatagorilerApi();
-    // api.getKategoriler();
-    api.getKatagoriler();
-    // print("api print$api");
-    String jsonText =
-        "[ { \"id\": 2, \"ad\": \"Icecekler\", \"aktif\": true }, { \"id\": 5, \"ad\": \"Yiyeceler\", \"aktif\": true }, { \"id\": 6, \"ad\": \"Yiyecelerd\", \"aktif\": true } ]";
+    // KatagorilerApi api = KatagorilerApi();
+    // // api.getKategoriler();
+    // api.getKatagoriler();
 
-    modelim = (jsonDecode(jsonText) as List)
-        .map((e) => KatagoriModel.fromJson(e))
-        .toList();
+    // print("api print$api");
+    // String jsonText =
+    //     "[ { \"id\": 2, \"ad\": \"Icecekler\", \"aktif\": true }, { \"id\": 5, \"ad\": \"Yiyeceler\", \"aktif\": true }, { \"id\": 6, \"ad\": \"Yiyecelerd\", \"aktif\": true } ]";
+
+    // modelim = (jsonDecode(jsonText) as List)
+    //     .map((e) => KatagoriModel.fromJson(e))
+    //     .toList();
     // for (var element in modelim) {
-    //   // print("${element.ad!}***");
+    //   print("${element.ad!}***");
     // }
+
+    // KatagorilerApi api = KatagorilerApi();
+    // api.getKatagoriler().then((value) {
+    //   // ignore: avoid_function_literals_in_foreach_calls
+    //   value.forEach((element) {
+    //     print("${element.ad!}+*");
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    // KatagorilerApi api = KatagorilerApi();
-    // api.getKatagoriler;
-    // List<KatagoriModel> modelli = [];
     return Scaffold(
         backgroundColor: Colors.grey,
         appBar: AppBar(
           toolbarHeight: 0,
         ),
-        body: Scrollbar(
-          thumbVisibility: true,
-          child: GridView.builder(
-              itemCount: modelim.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  mainAxisExtent: 150,
-                  crossAxisCount: 2),
-              itemBuilder: (BuildContext context, index) {
-                KatagoriModel model = modelim[index];
+        body: FutureBuilder<List<KatagoriModel>>(
+            future: api.getKatagoriler(),
+            builder: (context, snapshot) {
+              if (snapshot.data != null) {
+                List<KatagoriModel>? modelim = snapshot.data;
+                return _getKatagoriler(modelim);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                  strokeWidth: 2,
+                ),
+              );
+            }));
+  }
 
-                return Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Center(
-                    child: Column(
+  GridView _getKatagoriler(modelim) {
+    return GridView.builder(
+        itemCount: modelim.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            mainAxisExtent: 150,
+            crossAxisCount: 2),
+        itemBuilder: (BuildContext context, index) {
+          KatagoriModel model = modelim[index];
+
+          return Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(right: 10, top: 5),
+                    child: const Align(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.favorite),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 70,
+                    child: SizedBox(
+                        height: 150,
+                        width: 80,
+                        child: Image.asset("assets/download.jpg")),
+                  ),
+                  Expanded(
+                    flex: 30,
+                    child: Text("${modelim[index].ad} $index"),
+                  ),
+                  Expanded(
+                    flex: 20,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Container(
-                          padding: const EdgeInsets.only(right: 10, top: 5),
-                          child: const Align(
-                            alignment: Alignment.centerRight,
-                            child: Icon(Icons.favorite),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 70,
-                          child: SizedBox(
-                              height: 150,
-                              width: 80,
-                              child: Image.asset("assets/download.jpg")),
-                        ),
-                        Expanded(
-                          flex: 30,
-                          child: Text("${model.ad} $index"),
-                        ),
-                        Expanded(
-                          flex: 20,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              ElevatedButton(
-                                  onPressed: () {},
-                                  child: const Text("Sebete ekle")),
-                              IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.delete,
-                                    color: Colors.red,
-                                  ))
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 10)
+                        ElevatedButton(
+                            onPressed: () {
+                              KatagorilerApi api = KatagorilerApi();
+                              KatagoriModel modelim = KatagoriModel();
+                              modelim.ad = "denemeTest1ssss";
+                              modelim.aktif = true;
+                              modelim.id = 0;
+                              // model.id = 0;
+                              api.addKategoriler(modelim);
+                              // then((value) {
+                              //   print("veri eklendi$value");
+                              // setState(() {});
+                              // });
+                            },
+                            child: const Text("Sebete ekle")),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ))
                       ],
                     ),
                   ),
-                );
-              }),
-        ));
+                  const SizedBox(height: 10)
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/katagorilerApi.dart
+++ lib/katagorilerApi.dart
@@ -3,37 +3,44 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:loginsql/KatagorilerModel.dart';
 
+// ignore: avoid_print
+// print("status code ${response.statusCode}");
+// List jsonResponse = json.decode(response.body) as List;
+// ignore: avoid_print
+// return (jsonDecode(response.body) as List)
+//     .map((e) => KatagoriModel.fromJson(e))
+//     .toList();
+
 class KatagorilerApi {
-  String apiBaseUrl = "192.168.1.103:45455";
+  String apiBaseUrl = "192.168.1.117:45455";
   String path = "/api/Kategoriler/getKategoriler";
 
-  Future<List> getKatagoriler() async {
-    final response = await http.post(Uri.https(apiBaseUrl, path),
+  Future<List<KatagoriModel>> getKatagoriler() async {
+    final response = await http.get(Uri.https(apiBaseUrl, path),
         headers: {'Content-Type': 'application/json:charset=UTF-8'});
+    // String donenDeger = response.body.toString();
     if (response.statusCode == 200) {
-      // ignore: avoid_print
-      print("status code ${response.statusCode}");
-      // List jsonResponse = json.decode(response.body) as List;
-      // ignore: avoid_print
-      return (jsonDecode(response.body) as List)
-          .map((e) => KatagoriModel.fromJson(e))
-          .toList();
+      List jsonResponse = jsonDecode(response.body);
+      // print(response.statusCode);
+      // print(jsonResponse);
+      // List<KatagoriModel> model = [];
+      // print(donenDeger);
+      return jsonResponse.map((e) => KatagoriModel.fromJson(e)).toList();
     } else {
-      throw Exception("Error");
+      throw Exception("504");
     }
+    // List<KatagoriModel> modelim = (jsonDecode(donenDeger) as List)
+    //     .map((e) => KatagoriModel.fromJson(e))
+    //     .toList();
+    // return modelim;
   }
 
-  // modelim = (jsonDecode(jsonText) as List)
-  //     .map((e) => KatagoriModel.fromJson(e))
-  //     .toList();
-
   // Future<String> getKategoriler() async {
-
   //   String path = "/api/Kategoriler/getKategoriler";
   //   Uri uri = Uri.https(apiBaseUrl, path);
   //   var header = {'Content-Type': 'application/json:charset=UTF-8'};
 
-  //   return http.get(uri, headers: header).then((response) {
+  //   return http.post(uri, headers: header).then((response) {
   //     int statusCode = response.statusCode;
   //     print("status code $statusCode");
   //     String donenDeger = response.body.toString();
@@ -41,4 +48,20 @@ class KatagorilerApi {
   //     return "";
   //   });
   // }
+
+  Future<int> addKategoriler(KatagoriModel model) async {
+    var path = "/api/Kategoriler/setKategoriler";
+    Uri uri = Uri.https(apiBaseUrl, path);
+    var header = {'Content-Type': 'application/json'};
+    String jsonText = json.encode(model);
+    print("!!$jsonText");
+    return http.post(uri, headers: header, body: jsonText).then((responce) {
+      String jsonum = responce.body.toString();
+      if (responce.statusCode == 200) {
+        print("Hata");
+      }
+
+      return responce.statusCode;
+    });
+  }
 }
